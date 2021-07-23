b973a7dae2e50b9fe9dbcaa6009d248d1c62b1d5
litov74 <levt382@gmail.com>
2021-07-23 06:32:50 +0500

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
-import 'dart:html';
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:flutter/services.dart';
-import "package:test_project/screens/startup_screen.dart";
+import "package:wybe_latest/screens/startup_screen.dart";
 
-main() => runApp(Startup());
 
+// project entry point
+main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Startup()
+    );
+  }
+
+}
--- lib/screens/startup_screen.dart
+++ lib/screens/startup_screen.dart
@@ -1,24 +1,58 @@
+
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
+import "package:wybe_latest/strings/strings.dart";
 
-String appbar_text = "W"; // TODO: must be removed!
-String logo_asset = "assets/logo.jpg";
+const String logoAsset = "assets/logo.jpg";
 
 
-class Startup extends StatelessWidget{
+class Startup extends StatefulWidget{
+
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        //appBar: AppBar(
-        //  title: Text(appbar_text),
-        //),
-
-        body: Card(child: Column(children:<Widget>[
-
-          FittedBox(child: Image.asset(logo_asset), fit: BoxFit.fill),
-          //Text(appbar_text)
-        ],),),
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return StartState();
   }
 }
+
+class StartState extends State<Startup>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Colors.white,
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Image.asset(logoAsset, scale: 2,),
+            ),
+            Padding(padding: EdgeInsets.only(top: 20)),
+            Text(
+              loading,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 5)),
+            CircularProgressIndicator(
+              backgroundColor: Colors.grey,
+              strokeWidth: 1,
+            )
+          ],
+        ),
+      )
+
+
+    );
+
+
+
+  }
+  
+}
+
