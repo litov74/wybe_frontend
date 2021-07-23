6cce255d1523ea4136dd154a4781e3cfa84aea78
litov74 <levt382@gmail.com>
2021-07-23 06:33:33 +0500

    edited source file to make i18n

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test_project/main.dart';
-import 'package:test_project/screens/startup_screen.dart';
+import 'package:wybe_latest/main.dart';
+import 'package:wybe_latest/screens/startup_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
