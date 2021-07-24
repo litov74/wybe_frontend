d162fa1c6f7f8e2950b33fb270de55cdec051d60
litov74 <levt382@gmail.com>
2021-07-24 10:24:45 +0500

    reworked entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
+// GLOBAL DEPENDENCIES
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+// LOCAL DEPENDENCIES (note: project rename may cause an error, be careful)
 import "package:wybe_latest/screens/startup_screen.dart";
 
-
+String sql_statement = "CREATE TABLE user(id INTEGER PRIMARY KEY, name TEXT, phone TEXT, status TEXT, friends INTEGER, isRemember INTEGER)";
 // project entry point
-main() => runApp(MyApp());
+
+void main() async{
+  runApp(MyApp());
+  // running MyApp, then opening connection to DB. Hope, that connection won't be closed but I'm nut sure
+  WidgetsFlutterBinding.ensureInitialized();
+  final database = openDatabase(
+    join(await getDatabasesPath(), "user_database.db"),
+    onCreate: (db, version){
+      return db.execute(sql_statement,);
+    },
+    version: 1,
+  );
+}
 
 
 class MyApp extends StatelessWidget{
@@ -13,7 +30,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:Startup()
+      home:AnimatedGradient()
     );
   }
 
