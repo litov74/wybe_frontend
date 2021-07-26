c85d08c3dfff55634a8781a5eda0129a049afb5a
litov74 <levt382@gmail.com>
2021-07-26 19:08:13 +0500

    edited user model and DB

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,20 +1,22 @@
 import 'dart:ffi';
 
-class User{
+class Users{
   final int id;
   final String name;
   final String phone;
   final String status;
+  final String isLoggedIn;
   final int friends;
   final int isRemember; //BOOl type is unsupported in SQLite, using int...
   final String profilePicRoute;
   final Array friendsArray; // actually friends array is an array of User models
 
-  User({
+  Users({
     required this.id,
     required this.name,
     required this.phone,
     required this.status,
+    required this.isLoggedIn,
     required this.friends,
     required this.isRemember,
     required this.profilePicRoute,
